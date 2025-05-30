22d418f34de7f7e15bd7f34479856b1f3d0fda07
Jan Tadeusz Żukowski <4886712@teb-gdansk.ad>
2025-05-30 09:33:21 +0200

    update: improved styling

--- src/App.js
+++ src/App.js
@@ -38,9 +38,11 @@ function checkBoard(squares) {
 
 function Square({ value, update }) {
   return (
-    <button className="square" onClick={update}>
-      {value}
-    </button>
+    <td className="square">
+      <button onClick={update}>
+        {value}
+      </button>
+    </td>
   );
 }
 
@@ -53,7 +55,7 @@ export default function Board() {
       if(squares[idx]) {
         return;
       }
-      
+
       setSquares({ ...squares, [idx]: currentSymbol });
       setCurrentSymbol(currentSymbol === "X" ? "O" : "X");
     };
@@ -67,22 +69,22 @@ export default function Board() {
   }, [squares]);
 
   return (
-    <>
-      <div className="board-row">
+    <table className="board">
+      <tr className="board-row">
         <Square value={squares[0]} update={getClickHandler(0)} />
         <Square value={squares[1]} update={getClickHandler(1)} />
         <Square value={squares[2]} update={getClickHandler(2)} />
-      </div>
-      <div className="board-row">
+      </tr>
+      <tr className="board-row">
         <Square value={squares[3]} update={getClickHandler(3)} />
         <Square value={squares[4]} update={getClickHandler(4)} />
         <Square value={squares[5]} update={getClickHandler(5)} />
-      </div>
-      <div className="board-row">
+      </tr>
+      <tr className="board-row">
         <Square value={squares[6]} update={getClickHandler(6)} />
         <Square value={squares[7]} update={getClickHandler(7)} />
         <Square value={squares[8]} update={getClickHandler(8)} />
-      </div>
-    </>
+      </tr>
+    </table>
   );
 }
